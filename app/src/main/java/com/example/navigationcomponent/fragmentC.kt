8bca1057fdca5bcb427f35a253d50026638a8c21
Tanisha <tanishabhardwaj@zapbuild.com>
2021-09-13 17:29:27 +0530

    First Commit Sep

---
+++ app/src/main/java/com/example/navigationcomponent/fragmentC.kt
@@ -0,0 +1,30 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_c.view.*
+
+
+class fragmentC : Fragment() {
+    val args: fragmentCArgs by navArgs()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_c, container, false)
+
+        val textMessage = args.message
+        view.messageTextView.text= textMessage
+        return view
+    }
+
+
+
+    }
